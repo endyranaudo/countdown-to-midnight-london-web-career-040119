a727b3046949fde7ab60e52ae329d64b08970de7
Endy Ranaudo <endyranaudo@gmail.com>
2019-03-24 09:11:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,9 @@ def countdown(seconds)
   while seconds > 0
     puts "#{seconds} SECOND(S)!"
     seconds -= 1
-    def countdown_with_sleep
-      
+    def countdown_with_sleep(seconds)
+      sleep(seconds)
     end
   end
+  
 end
